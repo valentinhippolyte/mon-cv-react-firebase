fdf56bad5a5b3b6d1061abfa69a236b34ab0eae6
Valentin <valenthippo@gmail.com>
2022-04-02 20:47:00 +0200

    login logout ok !

--- src/components/ThisIsStatusAuth.js
+++ src/components/ThisIsStatusAuth.js
@@ -17,13 +17,39 @@ export function ThisIsStatusAuth(){
 
     if(!isSignedIn){
         return(
-            <Link style={{color: "white"}} to="/login">Login</Link>
+            // <Link style={{color: "white"}} to="/login"></Link>
+            <a href='login'><button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button></a>
         )
     }
     return(
         <div>
-            <p className="text-white">Bonjour {firebase.auth().currentUser.displayName}</p>
-            <button className="text-white" onClick={() => {firebase.auth().signOut()}}></button>
+            {/* <button className="text-white" onClick={() => {firebase.auth().signOut()}}>logout</button> */}
+            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={() => {firebase.auth().signOut()}}>Logout</button>
+
         </div>
     )
+}
+export function WelcomeTexte(){
+    const [isSignedIn, setIsSignedIn] = useState(false);
+
+    useEffect(() => {
+        const myStatusAuth = firebase.auth().onAuthStateChanged(user => {
+            setIsSignedIn(!!user);
+            console.log("user"+user);
+            console.log("is signes in"+isSignedIn);
+        })
+        return () => myStatusAuth();
+    }, []);
+
+     if(!isSignedIn){
+        return(
+            ''
+            )
+    }
+    return(
+        <p class="text-neutral-50	">
+            Bienvenue {firebase.auth().currentUser.displayName}
+        </p>
+    )
+
 }
